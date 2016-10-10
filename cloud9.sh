c2a85a669fe50160c052566966ed45a7d1f0cdea
Emory Merryman <emory.merryman@gmail.com>
2016-10-10 20:15:17 +0000

    use more tags

--- cloud9.sh
+++ cloud9.sh
@@ -3,9 +3,15 @@
 docker run --interactive --tty --rm \
     --env BIN_URL=git@github.com:endlesselectron/bin.git \
     --env BIN_TAG=0.0.0 \
-    --env PROJECT_UPSTREAM=git@github.com:tidyrailroad/cloud9.git \
-    --env PROJECT_ORIGIN=git@github.com:tidyrailroad/cloud9.git \
+    --env PROJECT_UPSTREAM=git@github.com:tidyrailroad/emacs.git \
+    --env PROJECT_ORIGIN=git@github.com:tidyrailroad/emacs.git \
     --env GIT_EMAIL=emory.merryman@gmail.com \
-    --env GIT_NAME="Emory Merryman"
+    --env GIT_NAME="Emory Merryman" \
     --volume dot_ssh:/root/.ssh \
-    --publish-all emorymerryman/cloud9:2.1.0
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --privileged \
+    --env DISPLAY \
+    --volume /tmp/.X11 \
+    --publish-all \
+    --env DOCKER_API_VERSION=1.22 \
+    emorymerryman/cloud9:2.1.3
