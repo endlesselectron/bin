5601b2f631a3714a312a2f22dcc797f3e38c8833
Emory Merryman <emory.merryman@gmail.com>
2016-10-10 08:56:14 +0000

    --net=host solves the problem of localhost not being ported, but it creates port conflicts when we try to do it multiple containers

--- cloud9.sh
+++ cloud9.sh
@@ -6,6 +6,11 @@ docker run --interactive --tty --rm \
     --env PROJECT_UPSTREAM=git@github.com:tidyrailroad/cloud9.git \
     --env PROJECT_ORIGIN=git@github.com:tidyrailroad/cloud9.git \
     --env GIT_EMAIL=emory.merryman@gmail.com \
-    --env GIT_NAME="Emory Merryman"
-    --volume dot_ssh:/root/.ssh \
-    --publish-all emorymerryman/cloud9:2.1.0
+    --env GIT_NAME="Emory Merryman" \
+    --env DISPLAY \
+    --volume dot_ssh:/root/.ssh:ro \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    --publish-all \
+    --net=host \
+    emorymerryman/cloud9:2.1.3
