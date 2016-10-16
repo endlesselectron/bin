2e71377d172c90af4abeee859f9e3761159b77a0
Emory Merryman <emory.merryman@gmail.com>
2016-10-16 11:04:53 +0000

    working on it

--- emacs.sh
+++ emacs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker run -it --rm --env DISPLAY --net=host --privileged emacs &&
+docker ps -q | while read CID
+    do
+        docker inspect ${CID} | grep "UUID="
+        true
+    done &&
+    docker run -it --rm --env DISPLAY --net=host --privileged emacs &&
     true
