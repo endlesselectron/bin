ccf0f636a753df9ea1414ac8beeac06d3b257d08
Emory Merryman <emory.merryman@gmail.com>
2016-10-30 21:34:56 +0000

    Detach cloud9, use bin 2.5.0

--- cloud9.sh
+++ cloud9.sh
@@ -6,7 +6,7 @@ docker \
     --tty \
     --detach \
     --env BIN_URL=git@github.com:endlesselectron/bin.git \
-    --env BIN_TAG=2.3.0 \
+    --env BIN_TAG=2.5.0 \
     --env PROJECT_UPSTREAM=${1} \
     --env PROJECT_ORIGIN=${2} \
     --env GIT_EMAIL=emory.merryman@gmail.com \
@@ -19,4 +19,4 @@ docker \
     --volume /tmp/.X11 \
     --publish-all \
     --env DOCKER_API_VERSION=1.22 \
-    emorymerryman/cloud9:2.3.0
+    emorymerryman/cloud9:2.3.1
